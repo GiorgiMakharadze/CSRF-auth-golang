7626e04c3d94f9de0743f27753413e95c4386ea4
GiorgiMakharadze <makharadzegiorgi00@gmail.com>
2024-07-04 20:27:31 +0400

    project init & models,middlewares

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/GiorgiMakharadze/CSRF-auth-golang/db"
+	"github.com/GiorgiMakharadze/CSRF-auth-golang/server"
+	"github.com/GiorgiMakharadze/CSRF-auth-golang/server/middleware/myJwt"
+)
+var host = "localhost"
+var port = "9000"
+
+func main() {
+	db.InitDB()
+
+	jwtErr := myJwt.InitJWT()
+	if jwtErr != nil {
+		log.Println("Error initializing the JWT")
+		log.Fatal(jwtErr)
+	}
+
+	serverErr := server.StartServer(host,port)
+	if serverErr != nil {
+		log.Println("Error starting server!")
+		log.Fatal(serverErr)
+	}
+}
